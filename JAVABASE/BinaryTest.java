4412f80744e54acc22ea65aa1c685365e84e54cb
YoonDii <dbswl9506@naver.com>
2023-02-26 16:36:39 +0900

    test

---
+++ JAVABASE/BinaryTest.java
@@ -0,0 +1,18 @@
+package JAVABASE;
+
+public class BinaryTest {
+    public static void main(String[] args){
+
+        int num = 10;
+        int bNum = 0B1010; // 2진수
+        int oNum = 012; // 8진수
+        int xNum = 0XA; // 16진수
+
+
+        System.out.println(num); //10
+        System.out.println(bNum); //10
+        System.out.println(oNum); //10
+        System.out.println(xNum); //10
+
+    }
+}
