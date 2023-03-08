799d5db338f4af3a25d56f9713bc98871a8cbada
YoonDii <dbswl9506@naver.com>
2023-03-08 10:45:58 +0900

    나도코딩퀴즈01

---
+++ JAVABASE/NaDo/_Quiz_01.java
@@ -0,0 +1,13 @@
+package JAVABASE.NaDo;
+
+public class _Quiz_01 {
+    public static void main(String[] args) {
+        String bus = "상암08";
+        int afterTime = 3;
+        double way = 1.2;
+
+        System.out.println(bus + "번 버스");
+        System.out.println("약" + " " + afterTime + "분 후 도착");
+        System.out.println("남은 거리" + " " + way +"km");
+    }
+}
