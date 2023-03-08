1843c329cc7915fa28af9729c6bc795804b4f8d4
YoonDii <dbswl9506@naver.com>
2023-03-08 11:33:06 +0900

    나도코딩퀴즈2

---
+++ JAVABASE/NaDo/_Quiz_02.java
@@ -0,0 +1,10 @@
+package JAVABASE.NaDo;
+
+public class _Quiz_02 {
+    public static void main(String[] args) {
+        int height = 115;
+
+        String result = (height >= 120) ? "탑승가능" : "탐승불가" ;
+        System.out.println("키가 "+height+"cm 이므로 "+ result);
+    }
+}
