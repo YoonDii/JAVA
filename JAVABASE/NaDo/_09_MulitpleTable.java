86568f0cd7e7c75f84e8970c3ae8a58b6609c8f8
YoonDii <dbswl9506@naver.com>
2023-06-25 18:37:41 +0900

    구구단

---
+++ JAVABASE/NaDo/_09_MulitpleTable.java
@@ -0,0 +1,13 @@
+package JAVABASE.NaDo;
+
+public class _09_MulitpleTable {
+    public static void main(String[] args) {
+        //구구단만들기
+        for (int i = 2; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                System.out.println(i+" x "+j+" = " +(i+j));
+            }
+            System.out.println();
+        }
+    }
+}
