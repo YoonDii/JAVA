67a22f1786b9d5d310dc910c4ab43b8326242508
YoonDii <dbswl9506@naver.com>
2023-06-25 18:29:48 +0900

    이중반복문

--- JAVABASE/NaDo/_08_NestedLoop.java
+++ JAVABASE/NaDo/_08_NestedLoop.java
@@ -19,5 +19,16 @@ public class _08_NestedLoop {
             }
             System.out.println();
         }
+        // 별 오른쪽 삼각형
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 4-i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <=i ; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
+
 }
